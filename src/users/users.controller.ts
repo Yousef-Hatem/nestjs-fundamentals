7cf3055539e4aac52ce6b252f5975b68bf892a3a
Yousef Hatem <yousef26hatem@gmail.com>
2023-05-26 09:56:45 +0300

    Find One And Create Logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,22 +11,32 @@ import {
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserEntity } from './user.entity';
+import { v4 as uuid } from 'uuid';
 
 @Controller('users')
 export class UsersController {
+  private readonly users: UserEntity[] = [];
+
   @Get()
-  find(): string[] {
-    return ['Ahmed', 'khalid', 'fatma'];
+  find(): UserEntity[] {
+    return this.users;
   }
 
-  @Get(':username')
-  findOne(@Param('username') username: string) {
-    return { username, email: 'ahmed@gmail.com' };
+  @Get(':id')
+  findOne(@Param('id') id: string): UserEntity {
+    return this.users.find((user) => user.id === id);
   }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return createUserDto;
+    const newUser: UserEntity = {
+      ...createUserDto,
+      id: uuid(),
+    };
+    this.users.push(newUser);
+
+    return newUser;
   }
 
   @Patch(':username')
